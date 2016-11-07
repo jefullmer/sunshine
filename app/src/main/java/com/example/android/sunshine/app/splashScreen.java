b25d3c50806fceba1dc4441238a21850a9a930a0
James <james.e.fullmer@gmail.com>
2016-11-06 21:02:51 -0700

    splash screen

---
+++ app/src/main/java/com/example/android/sunshine/app/splashScreen.java
@@ -0,0 +1,32 @@
+package com.example.android.sunshine.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+
+public class splashScreen extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Runnable waitTime = new Runnable()
+        {
+            public void run()
+            {
+                nextActivity();
+            }
+        };
+
+        Handler handler = new Handler();
+        handler.postDelayed(waitTime, 3500);
+    }
+    public void nextActivity()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
